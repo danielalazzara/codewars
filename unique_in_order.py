7eb8a24c882e52b8f7f8cae4e09c6f056767fdd8
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-08-20 13:26:04 +0100

    Create unique_in_order.py

---
+++ unique_in_order.py
@@ -0,0 +1,7 @@
+def unique_in_order(iterable):
+    result = []
+    for i in iterable:
+        if len(result) == 0 or i != result[len(result) -1]:
+            result.append(i)
+    return result
+  
