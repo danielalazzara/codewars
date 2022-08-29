62c3c9b755eb5328427173d27f1d18ee7aa06fe6
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-08-29 17:05:06 +0100

    Create delete_occurrences_of_an_element_if_it_occurs_more_than_n_times.py

---
+++ delete_occurrences_of_an_element_if_it_occurs_more_than_n_times.py
@@ -0,0 +1,9 @@
+def delete_nth(order,max_e):
+    new_order = []
+    for i in order:
+        if new_order.count(i) >= max_e:
+            continue
+        else:
+            new_order.append(i)
+    return new_order
+  
