2023a71428d76a03b628384a66d0fc4987e68340
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-09-09 14:52:31 +0100

    Create thinkful_logic_drills_traffic_light.py

---
+++ thinkful_logic_drills_traffic_light.py
@@ -0,0 +1,8 @@
+def update_light(current):
+    if current == 'green':
+        return 'yellow'
+    if current == 'yellow':
+        return 'red'
+    if current == 'red':
+        return 'green'
+      
