cbae9fe21c5c3a84bd8e82fc62cfb6844727a0ad
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-09-11 13:47:07 +0100

    Create quarter_of_the_year.py

---
+++ quarter_of_the_year.py
@@ -0,0 +1,10 @@
+def quarter_of(month):
+    if 1 <= month < 4:
+        return 1
+    elif 4 <= month < 7:
+        return 2
+    elif 7 <= month < 10:
+        return 3
+    else:
+        return 4
+      
