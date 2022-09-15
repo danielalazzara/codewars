30a0e56a8ec82b496e72fe94ccebf76ebe77e01c
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-09-15 19:31:03 +0100

    Create determine_offspring_sex_based_on_genes_xx_and_xy_chromosomes.py

---
+++ determine_offspring_sex_based_on_genes_xx_and_xy_chromosomes.py
@@ -0,0 +1,6 @@
+def chromosome_check(sperm):
+    if sperm == 'XX':
+        return "Congratulations! You're going to have a daughter."
+    if sperm == 'XY' or sperm == 'YX':
+        return "Congratulations! You're going to have a son."
+      
