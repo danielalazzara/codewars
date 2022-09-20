e47a32c0bf99e15360694cc73850cd4a4b4ecc15
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-09-20 18:36:16 +0100

    Create l1_bartender_drinks.py

---
+++ 8 kyu/l1_bartender_drinks.py
@@ -0,0 +1,16 @@
+def get_drink_by_profession(param):
+    if param.title() == "Jabroni":
+        return "Patron Tequila"
+    if param.title() == "School Counselor":
+        return "Anything with Alcohol"
+    if param.title() == "Programmer":
+        return "Hipster Craft Beer"
+    if param.title() == "Bike Gang Member":
+        return "Moonshine"
+    if param.title() == "Politician":
+        return "Your tax dollars"
+    if param.title() == "Rapper":
+        return "Cristal"
+    else:
+        return 'Beer'
+      
