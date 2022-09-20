a827224ec702601ebf279a1595ec76f2a5464981
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-09-20 19:02:23 +0100

    Update and rename total_amount_of_points.py to 8 kyu/total_amount_of_points.py

--- 8 kyu/total_amount_of_points.py
+++ 8 kyu/total_amount_of_points.py
@@ -1,15 +1,12 @@
 def points(games):
-    
     result = 0
     for i in games:
         x, y = i.split(':')
-        
         if x > y:
             result = result + 3
         elif x < y:
             result = result + 0
         elif x == y:
             result = result + 1
-    
     return result
   
