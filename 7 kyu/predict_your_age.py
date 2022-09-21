1e41f5f3adc1846a85bc57bb6391f1d52529fadf
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-09-21 10:18:22 +0100

    Create predict_your_age.py

---
+++ 7 kyu/predict_your_age.py
@@ -0,0 +1,3 @@
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+    return int((age_1 * age_1 + age_2 * age_2 + age_3 * age_3 + age_4 * age_4 + age_5 * age_5 + age_6 * age_6 + age_7 * age_7 + age_8 * age_8) ** 0.5) // 2
+  
