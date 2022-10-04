cf576c12dc5801dc2d490c513aeaa5acb596baf7
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-10-04 12:03:01 +0100

    Create define_a_card_suit.py

---
+++ 8 kyu/define_a_card_suit.py
@@ -0,0 +1,10 @@
+def define_suit(card):
+    if 'C' in card:
+        return 'clubs'
+    elif 'D' in card:
+        return 'diamonds'
+    elif 'H' in card:
+        return 'hearts'
+    else:
+        return 'spades'
+      
