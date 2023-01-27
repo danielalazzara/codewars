7ab9a729c6f95465297b44afd2444a97492b442a
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2023-01-27 11:22:54 +0000

    Create for_ufc_fans_total_beginners_conor_mcgregor_vs_george_saint_pierre.py

---
+++ 8 kyu/for_ufc_fans_total_beginners_conor_mcgregor_vs_george_saint_pierre.py
@@ -0,0 +1,6 @@
+def quote(fighter):
+    if fighter.lower() == ("George Saint Pierre").lower():
+        return "I am not impressed by your performance."
+    else:
+        return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+      
