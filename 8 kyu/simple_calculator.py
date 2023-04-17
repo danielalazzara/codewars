87ae469f9ef28719a5ca3e7e629570b0614abf77
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2023-04-17 15:17:52 +0100

    Create simple_calculator.py

---
+++ 8 kyu/simple_calculator.py
@@ -0,0 +1,12 @@
+def calculator(x,y,op):
+    if op == "+": 
+        return x + y
+    elif op == "-": 
+        return x - y
+    elif op == "*": 
+        return x * y
+    elif op == "/":
+        return x / y
+    else:
+        return 'unknown value'
+      
